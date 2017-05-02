b17da7c39b90bdafe8704f7f3898c9e7cf26ae25
Louis Randall <louisrandall@Louiss-MacBook-Pro.local>
2017-05-01 21:11:46 -0700

    set default values for images in portfolio model

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -15,7 +15,7 @@ class PortfoliosController < ApplicationController
     @portfolio_items = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
 
     respond_to do |format|
-      if @portfolio_item.save
+      if @portfolio_items.save
         format.html { redirect_to portfolios_path, notice: 'Your portfolio item is now live' }
       else
         format.html { render :new }
