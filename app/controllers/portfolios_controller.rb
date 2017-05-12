c56ededa6a042e518217a7b067d99c5777b5fdf4
Louis Randall <louisrandall@Louiss-MacBook-Pro.local>
2017-05-11 20:22:03 -0700

    configured custom layouts for blog and portfolio pages using partials and helpers

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,4 +1,5 @@
 class PortfoliosController < ApplicationController
+  layout "portfolio"
 	def index
 		@portfolio_items = Portfolio.all
 	end
@@ -25,7 +26,7 @@ class PortfoliosController < ApplicationController
   end
 
   def edit
-  	@portfolio_item = Portfolio.find(params[:id])
+  	@portfolio_items = Portfolio.find(params[:id])
   end
 
     def update
