ceadef7beac8679b80b94233053289c1a18bae7a
Louis Randall <louisrandall@Louiss-MacBook-Pro.local>
2017-06-13 19:45:23 -0700

    updated file upload forms and refactored place holder functionality into view

--- app/controllers/concerns/placeholder.rb
+++
@@ -1,8 +0,0 @@
-module Placeholder
-	extend ActiveSupport::Concern
-
-	def self.image_generator(height:, width:)
-		"http://placehold.it/#{height}X#{width}"
-
-	end
-end
--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -3,8 +3,8 @@ class Portfolio < ApplicationRecord
 has_many :technologies
 accepts_nested_attributes_for :technologies, reject_if: lambda { |attrs| attrs['name'].blank? }
 
-include Placeholder
-	validates_presence_of :title, :body, :main_image, :thumb_image
+
+	validates_presence_of :title, :body
 
 	mount_uploader :thumb_image, PortfolioUploader
 	mount_uploader :main_image, PortfolioUploader
@@ -17,11 +17,7 @@ include Placeholder
 		Portfolio.order("position ASC")
 	end
 
-	after_initialize :set_defaults
 
-	def set_defaults
-		self.main_image ||= Placeholder.image_generator(height: '600', width: '400')
-		self.thumb_image ||= Placeholder.image_generator(height: '350', width: '150')
-	end
 end
+
 
