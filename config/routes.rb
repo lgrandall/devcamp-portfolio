5ea2bc6c5716a80fd2d3df4214040af0971e93e5
Louis Randall <louisrandall@Louiss-MacBook-Pro.local>
2017-06-18 14:11:13 -0700

    created a page to host tweets and added the tweets to that page

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,8 @@ Rails.application.routes.draw do
   get 'portfolios/angular', to: 'portfolios#angular', as: 'portfolio_angular'
  
   get 'about', to: 'pages#about'
-  get 'contact', to: 'pages#contact'
+  get 'contact', to: 'pages#contact'  
+  get 'tech-news', to: 'pages#tech_news'
 
   resources :blogs do
   	member do
