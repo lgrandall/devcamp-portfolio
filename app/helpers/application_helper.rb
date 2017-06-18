b1053f5f3c31c1f765615a8ebe3335f3acb26ced
Louis Randall <louisrandall@Louiss-MacBook-Pro.local>
2017-06-18 12:40:53 -0700

    refactored alert system

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -59,6 +59,17 @@ module ApplicationHelper
     "active" if current_page? path
   end
 
+  def alerts
+    alert = (flash[:alert] || flash[:error] || flash[:notice])
+
+    if alert
+      alert_generator alert  
+    end
+  end
+
+  def alert_generator msg
+    js add_gritter(msg, title: "Louis Randall Portfolio", sticky: false, time: 3000)
+  end
 end
 
 
